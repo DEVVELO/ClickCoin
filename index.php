9aadada4a49b6539b205b8f0f1aa23863abdd304
TravianScripts <oliverweitman123@hotmail.com>
2015-10-05 16:49:37 +0200

    navigation
    fixed navigation

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <html> 
 <head>
 	<link rel="stylesheet" type="stylesheet/css" href="css/main.css">
-	
-
 </head>
 <body>
 <nav class="navbar navbar-default">
@@ -14,15 +12,18 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.php">CC</a>
+      <a class="navbar-brand" href="index.php">ClickCoin</a>
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
         <li><a href="contact.php">Contact</a></li>
+  
+      </ul>
+	  <ul class="nav navbar-nav navbar-right">
         <li><a href="login.php">Login</a></li>
 		<li><a href="register.php">Register</a></li>
-      </ul>
+	      </ul>
     </div>
   </div>
 </nav><center><br>
