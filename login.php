778c9bd2d5158e73a732f643b1de9cb82a564708
Hugo Krishan <hugo@krishan.se>
2015-10-05 17:06:07 +0200

    y0
    fe

---
+++ login.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	
+	<link rel="stylesheet" type="stylesheet/css" href="css/main.css">
+</head>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">ClickCoin</a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+	  <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="login.php">Login</a></li>
+		<li><a href="register.php">Register</a></li>
+	      </ul>
+    </div>
+  </div>
+</nav><center><br>
+	<h2>Login</h2>
+	<div class="progress" style="width:420px; height:3px;">
+	  <div class="progress-bar" style="width:100%;"></div>
+	</div><br><br>
+	<form method="POST" action="register.php">
+		<div class="form-group" style="width:300px;">
+		  <label class="control-label" for="inputDefault">Username</label>
+		  <input type="text" class="form-control" id="inputDefault"><br>
+		  <label class="control-label" for="inputDefault">Password</label>
+		  <input type="password" class="form-control" id="inputDefault"><br>
+		  <button type="reset" class="btn btn-default">Cancel</button>&nbsp;&nbsp;&nbsp;&nbsp;
+		  <button type="submit" class="btn btn-primary">Submit</button>
+		
+		
+		
+		
+		
+		</div>
+		
+		
+	</form>
+
+
+</center>
+</html>
