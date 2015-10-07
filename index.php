e3c2e18fd011dfce640ff0ece4f6efc2021691aa
TravianScripts <oliverweitman123@hotmail.com>
2015-10-07 17:51:53 +0200

    babom
    bitch

--- index.php
+++ index.php
@@ -27,75 +27,111 @@
     </div>
   </div>
 </nav><center><br>
-<h2>Welcome to Click Coin!</h2>
-	<div class="progress" style="width:420px; height:3px;">
-	  <div class="progress-bar" style="width:100%;"></div>
-	</div><br><br>
-	<div class="list-group">
-	  <a href="register.php" class="list-group-item" style="width:400px; position:absolute;left:11.5%;">
-	    <h4 class="list-group-item-heading">Why choose us?</h4><br>
-	    <p class="list-group-item-text">Click Coin is a website with diversity, which means that we have many different ways to earn money from. You can earn from clicking ads, making forms, doing captchas etc. Coin Clicker has also an extremely easy platform which provides a nice website where you as a member doesn't get confused.</p><br><p class="text-danger">Press this box inorder to register!</p>
-	  </a><div class="list-group">
-	  <a href="register.php" class="list-group-item" style="width:400px; position:absolute;left:36%;">
-    <h4 class="list-group-item-heading">Why choose us?</h4><br>
-    <p class="list-group-item-text">Click Coin is a website with diversity, which means that we have many different ways to earn money from. You can earn from clicking ads, making forms, doing captchas etc. Coin Clicker has also an extremely easy platform which provides a nice website where you as a member doesn't get confused.</p><br><p class="text-danger">Press this box inorder to register!</p>
-		</p>
-	  </a><a href="register.php" class="list-group-item" style="width:400px; position:absolute;left:66%;">
-
-    <h4 class="list-group-item-heading">Why choose us?</h4><br>
-    <p class="list-group-item-text">Click Coin is a website with diversity, which means that we have many different ways to earn money from. You can earn from clicking ads, making forms, doing captchas etc. Coin Clicker has also an extremely easy platform which provides a nice website where you as a member doesn't get confused.</p><br><p class="text-danger">Press this box inorder to register!</p>
-	  </a>
-	  
-	  
-	  
-	  <br><br><br><br><br><br><br><br><br><br><br><br>
- 	<div class="progress" style="width:77%; height:3px;">
- 	  <div class="progress-bar" style="width:100%;"></div>
- 	</div><br>
-	
-	<div class="panel panel-primary" style="width:20%; position:absolute;left:29.6%;">
-	  <div class="panel-heading">
-	    <h3 class="panel-title">Top Earners Today</h3>
-	  </div>
-	  <div class="panel-body">
-
-		  <ol >
-	   
-
-	    
-		<li style="position:flex;left:1%;">User 1 has earned $1000   </li>
-		
-		<li style="position:flex;left:1%;">User 2 has earned $500  </li>
-		
-		<li style="position:flex;left:1%;">User 3 has earned $250  </li>
-		
-		<li style="position:flex;left:1%;">User 4 has earned $150  </li>
-		
-		<li style="position:flex;left:1%;">User 5 has earned $99  </li>
-	
-
-	  </div>
-	</div>
-	<div class="panel panel-success" style="width:20%; position:absolute;left:51.0%;">
-	  <div class="panel-heading">
-	    <h3 class="panel-title">News</h3>
-	  </div>
-	  <div class="panel-body">
-	    <p>
-	We just launched our service, you may now use our application smothly and start earning your cash. We offer you an
-	amazing opportunity to earn money online by just clicking!</p>
-	
-	  </div>
-	</div>
-
+<?php
+//Processing data area
 	
 	
-	</div><br><br><br><br><br><br><br><br><br>
-	<h4>Coin Clicker has <u>1000</u> members and we have paid our members <u>860$</u></h4>
-<br><br><br>
-<p class="text-muted" style="overflow:hidden;position:flex;bottom:0%;"><b>Copyright&copy; 2015 ClickCoin</b></p>
-</center>
-</footer>
-</center>
+?>
+<h1>Welcome to 
+CoinClaimer!</h1>
+<big>Earn Coins Without Mining!</big>
+<hr>
+</div>
+
+<!--Coinpics-->
+<h3>We currently support these coins for withdraws</h3>
+<ul style="list-style-type:none">
+	<li style="display:inline"><img src="images/Bitcoin.png" alt="Bitcoin" height="50" width="50"></li>
+	<li style="display:inline"><img src="images/Dogecoin.png" alt="Dogecoin" height="50" width="50"></li>
+	<li style="display:inline"><img src="images/Litecoin.png" alt="Litecoin" height="50" width="50"></li>
+	<li style="display:inline"><img src="images/Darkcoin.png" alt="Darkcoin" height="50" width="50"></li>
+	<li style="display:inline"><img src="images/Peercoin.png" alt="Peercoin" height="50" width="50"></li>
+	<li style="display:inline"><img src="images/Primecoin.png" alt="Primecoin" height="50" width="50"></li>
+	<li style="display:inline"><img src="images/Reddcoin.png" alt="Reddcoin" height="50" width="50"></li>
+	<li style="display:inline"><img src="images/Mintcoin.png" alt="Mintcoin" height="50" width="50"></li>
+        <li style="display:inline"><img src="images/shadowcoin.png" alt="Shadowcoin" height="55" width="55"></li>
+</ul>
+<!--EndCoinpics-->
+
+<style>
+@media only screen and (max-device-width: 480px) {
+div#bar1{
+	position:flex;left:17%;top:45%;
+}	
+}
+</style>
+<div class="row">
+<div class="col-md-6" style="" id="bar1">
+<div class="panel panel-primary" style="width:700px;">
+<div class="panel-body" >
+<div align="center" >
+	
+<h3><b>What is CoinClaimer?</b></h3>
+<br>
+
+
+<br>
+CoinClaimer is a free service where you earn points for various activities. You can earn 24 coins per hour just from filling in a captcha. In the future there will be even more ways to earn such as link shrinking and more that we are working to integrate. You may exchange your points for a variety of different coins. We currently support 9 different coins, we are always working on adding more coins and more features to our site. Stay tuned for more!
+<br>
+<br>
+<b>We hope you are convinced to try our service!</b>
+<br><br>
+<a href="register" class="btn btn-danger">Register and start earning</a>
+</div>
+</div>
+</div>
+</div>
+<style>
+@media only screen and (max-device-width: 480px) {
+div#bar{
+	position:absolute;left:16%;top:90%;
+}	
+}
+</style>
+<div class="col-md-6" style="" id="bar">
+<div class="panel panel-primary" style="width:700px;">
+<div class="panel-body">
+<div align="center">
+<h3><b>How can we give out free coins?</b></h3>
+
+
+<p>We don't give out free coins. We earn on advertising and captcha filling. Everytime you complete a captcha we earn a little bit of money that you will be paid for. We are also earning our income through services such as shorte.st that helps us set higher rates per point, we also have a few banner ads so please don't see on them as annoying instead see them as a better way to earn more yourself! <p>Well trying something new and free isn't a harm right?</p>
+
+<br>
+<br>
+
+<b>We hope you are convinced to try our service!</b>
+<br>
+<br>
+<a href="register" class="btn btn-danger">Register and start earning</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div align="center">
+Banner ad space<br><br><br>
+<h4>We have 3 users and 0 paid withdraws</h4>
+</div>
 </body>
+<div id="br">
+</div>
+<br><style>
+@media only screen and (max-device-width: 480px) {
+	div#br{
+		white-space: pre-line;
+		height:900px;
+	}
+}
+</style>
+<footer class="footer" style="position:flex; bottom:0%; overflow:hidden;width:100%;height:70;">
+<div class="well">
+<div align="center">
+<p class="text-muted"><b>Copyright&copy; 2015 CoinClaimer</b></p>
+</div>
+</div>
+</footer>
+
 </html>
